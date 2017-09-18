e98f1c0552eadd8306c5bfa5c5fcfb58a822bfcb
Arvid Boivie <arvid.boivie@gmail.com>
2017-09-18 08:54:38 +0200

    Fix scopes

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -27,7 +27,7 @@ class AuthController
 
         $authorizeUrl = $apiSession->getAuthorizeUrl([
             'scope' => [
-                implode(',', $spotifyConfig['scopes']),
+                $spotifyConfig['scopes'],
             ]
         ]);
 
